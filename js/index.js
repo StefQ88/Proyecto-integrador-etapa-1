2b56993c36c31c30381fd66bf07f42f68fcd68b8
Stefani Quiolla <stefiquio@gmail.com>
2025-05-04 18:21:13 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+/**
+ * Alterna la visibilidad del menú móvil.
+ * 
+ * Esta función se activa al hacer clic en el botón de menú hamburguesa.
+ * Añade o elimina la clase `menu-mobile-visible` al elemento con ID `mobile_menu`,
+ * lo que permite mostrar u ocultar el menú en pantallas pequeñas.
+ * 
+ * También cambia el estado booleano `showMenu` para saber si el menú está abierto o no.
+ */
+
+let showMenu = false
+
+const handleMenu = () => {
+
+    const menu = document.getElementById("mobile_menu")
+    const header = document.querySelector(".header")
+    const footer = document.querySelector(".footer")
+
+    if (!menu || !header || !footer) return; //si alguno no existe salimos de la funcion
+
+    const headerHeight = header.offsetHeight; //altura real de header en px
+    const footerHeight = footer.offsetHeight;
+
+    const windowHeight = window.innerHeight; //altura visible de la ventana
+
+
+    if (!showMenu) {
+        menu.classList.add("menu__container-visible") //agrego la clase que lo muestre
+
+        menu.style.top = `${headerHeight}px` //debajo del header
+
+        menu.style.height = `${windowHeight - headerHeight}px` //altura dinamica
+
+        document.body.style.overflow = "hidden" //evito scroll
+
+    } else {
+        menu.classList.remove("menu__container-visible") 
+
+        menu.style.top = ""
+        menu.style.height = ""
+
+        document.body.style.overflow = "auto" // permitimos scroll
+    }
+    showMenu = !showMenu
+
+}
